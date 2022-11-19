5b7f2405e107ca7c469a385710a8d72202d119d9
ABHISHEK THAKUR <99659192+Nobita-XD@users.noreply.github.com>
2022-11-19 22:27:03 +0530

    Update new_join_fn.py

--- bot/plugins/new_join_fn.py
+++ bot/plugins/new_join_fn.py
@@ -3,12 +3,12 @@
 # (c) Shrimadhav U K | @AbirHasan2005
 
 
-from bot.database import Database
+from bot.database import Database, SESSION_NAME
 from bot.localisation import Localisation
 from bot import (
     UPDATES_CHANNEL,
-    DATABASE_URL,
-    SESSION_NAME
+    DATABASE_URL
+    
 )
 from pyrogram.types import ChatPermissions, InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, UsernameNotOccupied, ChatAdminRequired, PeerIdInvalid
